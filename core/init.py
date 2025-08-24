394f5a95e3d8e7c796932600818ef14f00cf7974
Alen Pepa <xalenpepa@gmail.com>
2025-08-24 06:11:09 -0700

    Create init.py

---
+++ core/init.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Utilities Package - Network Security Scanner Suite
+Helper functions and utilities
+"""
+
+__version__ = "1.0.0"
+__author__ = "Alen Pepa"
+
+__all__ = []
