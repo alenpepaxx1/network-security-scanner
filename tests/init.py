5f7cd42ba6da0dd3c272612380bf3a9a8f0baaab
Alen Pepa <xalenpepa@gmail.com>
2025-08-24 06:14:50 -0700

    Create init.py

---
+++ tests/init.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test Package - Network Security Scanner Suite
+Unit tests and integration tests
+"""
+
+__version__ = "1.0.0"
+__author__ = "Alen Pepa"
